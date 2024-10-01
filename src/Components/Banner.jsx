0bf610868a1a25578eb84235978a298b87337e47
marnf01~ <marnf91@gmail.com>
2024-10-01 09:33:30 -0600

    4th commit

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FaArrowRight } from "react-icons/fa6";
 import image2 from "../assets/pic2.png"
 import CountShower from './CountShower';
 
 
 const Banner = () => {
+
+
+    useEffect(() => {
+        const moveObject = (id, animationClass) => {
+            const element = document.getElementById(id);
+            element.classList.add(animationClass);
+
+        };
+
+        moveObject("banner-picture", "animate-move-left");
+    }, []);
+
+
     return (
         <div className='w-full bg-blue-100 m-auto'>
             <div className='md:flex items-center m-auto justify-center gap-5'>
@@ -15,7 +28,7 @@ const Banner = () => {
                     <p>worldwide</p>
                     <button className='border rounded flex items-center justify-center gap-3 bg-blue-600 p-3'>create an account <FaArrowRight /></button>
                 </div>
-                <div>
+                <div id='banner-picture'>
                     <img src={image2} alt="" />
                 </div>
 
--- src/Components/VideoPage.jsx
+++ src/Components/VideoPage.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ScrollTrigger from 'react-scroll-trigger';
 
 const VideoPage = () => {
+
+    const [moveState, setMoveState] = useState(false)
+
+    useEffect(() => {
+        const moveObject = (id, animationClass) => {
+            const element = document.getElementById(id);
+            if(element && moveState){
+                element.classList.add(animationClass)
+            }
+            else if( element){
+                element.classList.remove(animationClass)
+            }
+
+        };
+
+        moveObject("video-banner", "animate-move-right");
+    }, [moveState]);
+
+
     return (
-        <div className='bg-blue-300 m-auto p-3'>
+        <div  className='bg-blue-300 m-auto p-3'>
             <h1 className='text-center text-4xl p-7'>Find your perfect study destination</h1>
             <div className='justify-center p-4'>
-                <video className='m-auto rounded-2xl' width="600" height="400" controls>
+
+            <ScrollTrigger onEnter={() => setMoveState(true)} onExit={() => setMoveState(false)}>
+            <video id='video-banner' className={`m-auto rounded-2xl ${moveState ? 'animate-move-right' : ''}`} width="600" height="400" controls>
                     <source src="" type="video/mp4" />
 
                 </video>
+                </ScrollTrigger>
             </div>
         </div>
     );
